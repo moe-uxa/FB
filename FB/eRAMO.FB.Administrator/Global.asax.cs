5c1fbe2c11847144b5ab076f157918bfb4afb97c
moe-uxa <m.usama.said@live.com>
2015-10-03 20:51:10 +0200

    -

--- FB/eRAMO.FB.Administrator/Global.asax.cs
+++ FB/eRAMO.FB.Administrator/Global.asax.cs
@@ -13,7 +13,7 @@ namespace eRAMO.Administrator
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        { 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
