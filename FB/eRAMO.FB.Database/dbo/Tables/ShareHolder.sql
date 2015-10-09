2082ce111283c03098a4610c5b7e4445664625d4
moe-uxa <m.usama.said@live.com>
2015-10-09 21:11:29 +0200

    ShareHolder DB Table

---
+++ FB/eRAMO.FB.Database/dbo/Tables/ShareHolder.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ShareHolder] (
+    [ShareHolderID] INT             IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (50)   NULL,
+    [Title]         NVARCHAR (50)   NULL,
+    [Summary]       NVARCHAR (4000) NULL,
+    [PhotoUrl]      NVARCHAR (4000) NULL,
+    [FacebookUrl]   NVARCHAR (4000) NULL,
+    [TwitterUrl]    NVARCHAR (4000) NULL,
+    [GooglePlusUrl] NVARCHAR (4000) NULL,
+    [LinkedInUrl]   NVARCHAR (4000) NULL,
+    [IsCEO]         BIT             NULL,
+    [Rank]          INT             NULL,
+    CONSTRAINT [PK_ShareHolder] PRIMARY KEY CLUSTERED ([ShareHolderID] ASC)
+);
+
