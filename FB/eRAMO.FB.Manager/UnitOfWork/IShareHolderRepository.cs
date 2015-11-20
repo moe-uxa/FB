7ec00e1e281ca3a9837a10b4ac51d71e57f12fbe
moe-uxa <m.usama.said@live.com>
2015-11-20 21:07:28 +0200

    -

---
+++ FB/eRAMO.FB.Manager/UnitOfWork/IShareHolderRepository.cs
@@ -0,0 +1,7 @@
+using eRAMO.FB.Data.Model;
+using eRAMO.FB.Data;
+
+public interface IShareHolderRepository : IRepository<eRAMO.FB.Data.Model.ShareHolder, eRAMO.FB.Data.Model.IShareHolder>
+{
+    //Add any additional repository methods other than the generic ones (GetAll, GetById, Delete, Add)
+}
---
+++ FB/eRAMO.FB.Manager/UnitOfWork/ShareHolderRepository.cs
@@ -0,0 +1,18 @@
+
+    
+using eRAMO.FB.Data.Model;
+using eRAMO.FB.Data;
+using System.Collections.Generic;
+using System.Linq;
+        
+public class ShareHolderRepository : Repository<eRAMO.FB.Data.Model.ShareHolder, eRAMO.FB.Data.Model.IShareHolder>, IShareHolderRepository
+{
+    private FBEntities _context;
+
+    public ShareHolderRepository(FBEntities context) : base(context)
+    {
+        _context = context;
+    }
+
+    //Override any generic method for your own custom implemention, add new repository methods to the IShareHolderRepository.cs file
+}
